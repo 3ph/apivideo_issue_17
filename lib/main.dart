bdcba12fd7d3c9483bfeaa397fea0030d8295362
Tom Friml <instantni.med@gmail.com>
2022-10-20 11:12:32 +1300

    Adds api video and screen rotating functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:apivideo_live_stream/apivideo_live_stream.dart';
 import 'package:flutter/material.dart';
+import 'package:device_orientation/device_orientation.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,106 +16,136 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return const MaterialApp(home: StreamPreviewWidget());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class StreamPreviewWidget extends StatefulWidget {
+  const StreamPreviewWidget({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StreamPreviewWidget> createState() => _StreamPreviewWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _StreamPreviewWidgetState extends State<StreamPreviewWidget> {
+  StreamSubscription? _orientationSubscription;
+  bool _shouldLockPortrait = true;
+  DeviceOrientation _deviceOrientation = DeviceOrientation.portraitUp;
+  late LiveStreamController _controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    _controller = LiveStreamController(
+      onConnectionFailed: (_) {
+        debugPrint('Stream connecton failed');
+      },
+      onConnectionSuccess: () {
+        debugPrint('Stream connected');
+      },
+      onDisconnection: () {
+        debugPrint('Server disconnect');
+      },
+    );
+    _controller.create(
+      initialAudioConfig: AudioConfig(),
+      initialVideoConfig: VideoConfig.withDefaultBitrate(),
+    );
+
+    Future.delayed(Duration.zero, () {
+      _forceOrientation(
+          shouldLockPortrait:
+              MediaQuery.of(context).orientation == Orientation.portrait);
     });
+    _orientationSubscription = deviceOrientation$.listen((orientation) {
+      _deviceOrientation = orientation.fixed;
+      print('Device orientation: $_deviceOrientation');
+      _forceOrientation();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _orientationSubscription?.cancel();
+    _restoreOrientation();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(),
+      body: CameraPreview(controller: _controller),
+      floatingActionButton: IconButton(
+        icon: const Icon(Icons.crop_rotate),
+        onPressed: () {
+          _forceOrientation(shouldLockPortrait: !_shouldLockPortrait);
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<void> _forceOrientation({bool? shouldLockPortrait}) async {
+    if (shouldLockPortrait != null) {
+      _shouldLockPortrait = shouldLockPortrait;
+      // force orientation button was pressed
+      // check if we are in correct device orientation
+      if ((shouldLockPortrait && _deviceOrientation.isPortrait) ||
+          !(shouldLockPortrait || _deviceOrientation.isPortrait)) {
+        await SystemChrome.setPreferredOrientations([
+          _deviceOrientation,
+        ]);
+        print('Forcing orientation1: $_deviceOrientation');
+      } else {
+        // use defaults
+        await SystemChrome.setPreferredOrientations([
+          shouldLockPortrait
+              ? DeviceOrientation.portraitUp
+              : DeviceOrientation.landscapeRight,
+        ]);
+        print(
+            'Forcing orientation2: ${shouldLockPortrait ? DeviceOrientation.portraitUp : DeviceOrientation.landscapeRight}');
+      }
+    } else {
+      // native orientation changed, check if we are in correct lock orientation
+      // otherwise ignore
+      if ((_shouldLockPortrait && _deviceOrientation.isPortrait) ||
+          !(_shouldLockPortrait || _deviceOrientation.isPortrait)) {
+        await SystemChrome.setPreferredOrientations([
+          _deviceOrientation,
+        ]);
+        print('Forcing orientation3: $_deviceOrientation');
+      }
+    }
+  }
+
+  Future<void> _restoreOrientation() async {
+    await SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+}
+
+extension OrientationExtensions on DeviceOrientation {
+  bool get isPortrait =>
+      this == DeviceOrientation.portraitDown ||
+      this == DeviceOrientation.portraitUp;
+
+  DeviceOrientation get fixed {
+    if (Platform.isIOS) return this;
+    // Landscape is swapped on Android??
+    switch (this) {
+      case DeviceOrientation.portraitUp:
+      case DeviceOrientation.portraitDown:
+        return this;
+
+      case DeviceOrientation.landscapeLeft:
+        return DeviceOrientation.landscapeRight;
+      case DeviceOrientation.landscapeRight:
+        return DeviceOrientation.landscapeLeft;
+    }
   }
 }
